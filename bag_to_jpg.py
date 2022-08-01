350e9e8a12fbe64e763db7c9e3903b2ff109b3c0
Bartosz Ptak <bartptak@gmail.com>
2022-08-01 09:19:54 +0200

    Add tools

---
+++ bag_to_jpg.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+import click
+import cv2
+import numpy as np
+import rosbag as rsb
+
+
+@click.command()
+@click.option('--rosbag', default=None, type=str, help='Path to rosbag', required=True)
+@click.option('--topic', default='/camera/rgb/image_raw', type=str, help='Image topic name', required=True)
+def main(rosbag: str, topic: str):
+    bag = rsb.Bag(rosbag, "r")
+    
+    dest = Path(os.path.join('outputs', rosbag.split('/')[-1].split('.')[0]))
+    dest.mkdir(exist_ok=True)
+    
+    for topic_name, msg, t in bag.read_messages(topics=[topic]):        
+        if topic_name != topic:
+            continue
+        
+        img = np.frombuffer(
+                msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1).copy()
+        
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+        cv2.imwrite(str(dest) + f'/{t}.jpg', img)  
+    
+        
+    cv2.destroyAllWindows()
+    bag.close()
+    
+if __name__ == '__main__':
+    main()
+    
---
+++ mp4_to_jpg.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+import click
+import cv2
+import numpy as np
+
+
+@click.command()
+@click.option('--video', default=None, type=str, help='Path to rosbag', required=True)
+
+def main(video: str):
+    cap = cv2.VideoCapture(video)
+    
+    dest = Path(os.path.join('outputs', video.split('/')[-1].split('.')[0]))
+    dest.mkdir(exist_ok=True)
+    
+    i = 0
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+
+        if i%30 == 0:
+            cv2.imwrite(str(dest) + f'/{i:05d}.jpg', frame)  
+    
+        i+=1
+        
+    cv2.destroyAllWindows()
+    
+if __name__ == '__main__':
+    main()
+    
